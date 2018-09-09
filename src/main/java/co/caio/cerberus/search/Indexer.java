64b01dc7c3b7a54f8882d968ede71d2af759e27b
Caio Romão <hello@caio.co>
2018-09-09 10:52:49 +0200

    Make Indexer aware of taxonomies
    This patch makes the Indexer implementation contain a configuration
    for writing taxonomies. Todo:
    
    * Actually write the taxonomies
    * Make the searcher aware of it all

--- src/main/java/co/caio/cerberus/search/Indexer.java
+++ src/main/java/co/caio/cerberus/search/Indexer.java
@@ -4,13 +4,19 @@ import co.caio.cerberus.model.Recipe;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
+import org.apache.lucene.facet.FacetsConfig;
+import org.apache.lucene.facet.taxonomy.TaxonomyWriter;
+import org.apache.lucene.facet.taxonomy.directory.DirectoryTaxonomyWriter;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.OptionalInt;
 
@@ -19,6 +25,7 @@ import static co.caio.cerberus.search.IndexField.*;
 public interface Indexer {
     void addRecipe(Recipe recipe) throws IOException;
 
+    // TODO get rid of this
     Directory getDirectory();
 
     int numDocs();
@@ -29,36 +36,35 @@ public interface Indexer {
 
     class Builder {
         private Directory indexDirectory;
+        private Directory taxonomyDirectory;
         private Analyzer analyzer;
         private IndexWriterConfig writerConfig;
         private IndexWriterConfig.OpenMode openMode;
 
+        private static Logger logger = LoggerFactory.getLogger(Builder.class);
+        // XXX This should be somewhere more accessible since the searcher also needs it
+        private static String INDEX_DIR_NAME = "index";
+        private static String TAXONOMY_DIR_NAME = "taxonomy";
+
         public Builder reset() {
             indexDirectory = null;
+            taxonomyDirectory = null;
             analyzer = null;
             writerConfig = null;
             openMode = null;
             return this;
         }
 
-        private Builder indexDirectory(Directory dir) {
-            indexDirectory = dir;
+        public Builder dataDirectory(Path dir) {
+            indexDirectory = openDirectory(dir.resolve(INDEX_DIR_NAME));
+            taxonomyDirectory = openDirectory(dir.resolve(TAXONOMY_DIR_NAME));
             return this;
         }
 
-        public Builder indexDirectory(Path dir) {
-            if (! dir.toFile().isDirectory()) {
-                throw new IndexBuilderException(String.format("%s is not a directory", dir));
-            }
-            try {
-                return indexDirectory(FSDirectory.open(dir));
-            } catch (Exception e) {
-                throw new IndexBuilderException(String.format("Exception opening %s: %s", dir, e));
-            }
-        }
-
-        public Builder inMemoryIndex() {
-            return indexDirectory(new RAMDirectory());
+        public Builder inMemory() {
+            indexDirectory = new RAMDirectory();
+            taxonomyDirectory = new RAMDirectory();
+            return this;
         }
 
         public Builder analyzer(Analyzer an) {
@@ -83,6 +89,25 @@ public interface Indexer {
             return openMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
         }
 
+        private Directory openDirectory(Path dir) {
+            if (! dir.toFile().exists() && dir.getParent().toFile().isDirectory()) {
+                logger.debug("Creating directory %s", dir);
+                try {
+                    Files.createDirectory(dir);
+                } catch (IOException wrapped) {
+                    throw new IndexBuilderException(wrapped.getMessage());
+                }
+            }
+            if (! dir.toFile().isDirectory()) {
+                throw new IndexBuilderException(String.format("%s is not a directory", dir));
+            }
+            try {
+                return FSDirectory.open(dir);
+            } catch (Exception e) {
+                throw new IndexBuilderException(String.format("Exception opening %s: %s", dir, e));
+            }
+        }
+
         public Indexer build() {
             if (analyzer == null) {
                 analyzer = new StandardAnalyzer();
@@ -97,13 +122,19 @@ public interface Indexer {
             }
 
             if (indexDirectory == null) {
-                throw new IndexBuilderException("Missing `directory`");
+                throw new IndexBuilderException("Missing `indexDirectory`");
+            }
+
+            if (taxonomyDirectory == null) {
+                throw new IndexBuilderException("Missing `taxonomyDirectory`");
             }
 
             writerConfig.setOpenMode(openMode);
 
             try {
-                return new IndexerImpl(new IndexWriter(indexDirectory, writerConfig));
+                return new IndexerImpl(
+                        new IndexWriter(indexDirectory, writerConfig),
+                        new DirectoryTaxonomyWriter(taxonomyDirectory, openMode));
             } catch (IOException e) {
                 throw new IndexBuilderException(String.format("Failure creating index writer: %s", e));
             }
@@ -111,14 +142,19 @@ public interface Indexer {
 
         private class IndexerImpl implements Indexer {
             private final IndexWriter indexWriter;
+            private final TaxonomyWriter taxonomyWriter;
+            private final FacetsConfig facetsConfig;
 
-            private IndexerImpl(IndexWriter writer) {
+            private IndexerImpl(IndexWriter writer, TaxonomyWriter taxWriter) {
                 indexWriter = writer;
+                taxonomyWriter = taxWriter;
+                facetsConfig = new FacetsConfig();
             }
 
             @Override
             public void addRecipe(Recipe recipe) throws IOException {
                 var doc = new Document();
+
                 doc.add(new StoredField(RECIPE_ID, recipe.recipeId()));
                 doc.add(new LongPoint(SITE_ID, recipe.siteId()));
                 doc.add(new StringField(CRAWL_URL, recipe.crawlUrl(), Field.Store.YES));
@@ -141,7 +177,8 @@ public interface Indexer {
                 addOptionalIntField(doc, CARBOHYDRATE_CONTENT, recipe.carbohydrateContent());
                 addOptionalIntField(doc, FAT_CONTENT, recipe.fatContent());
                 addOptionalIntField(doc, PROTEIN_CONTENT, recipe.proteinContent());
-                indexWriter.addDocument(doc);
+
+                indexWriter.addDocument(facetsConfig.build(taxonomyWriter, doc));
             }
 
             private void addOptionalIntField(Document doc, String fieldName, OptionalInt value) {
@@ -163,11 +200,13 @@ public interface Indexer {
             @Override
             public void close() throws IOException {
                 indexWriter.close();
+                taxonomyWriter.close();
             }
 
             @Override
             public void commit() throws IOException {
                 indexWriter.commit();
+                taxonomyWriter.commit();
             }
 
         }
--- src/test/java/co/caio/cerberus/search/IndexerTest.java
+++ src/test/java/co/caio/cerberus/search/IndexerTest.java
@@ -19,27 +19,27 @@ class IndexerTest {
                 () -> assertThrows(exc, builder::build),
                 () -> assertThrows(exc, () -> builder.reset().createMode().build()),
                 () -> assertThrows(exc, () -> builder.reset().analyzer(new StandardAnalyzer()).build()),
-                () -> assertThrows(exc, () -> builder.reset().indexDirectory(Paths.get("void")).createMode().build()),
-                () -> assertThrows(exc, () -> builder.reset().inMemoryIndex().appendMode().build())
+                () -> assertThrows(exc, () -> builder.reset().dataDirectory(Paths.get("void")).createMode().build()),
+                () -> assertThrows(exc, () -> builder.reset().inMemory().appendMode().build())
         );
     }
 
     @Test
     public void simpleLocalIndexer() throws IOException {
         var tempDir = Files.createTempDirectory("cerberus-test");
-        var index = new Indexer.Builder().indexDirectory(tempDir).createOrAppendMode().build();
+        var index = new Indexer.Builder().dataDirectory(tempDir).createOrAppendMode().build();
         assertEquals(0, index.numDocs());
         index.addRecipe(Util.getBasicRecipe());
         assertEquals(1, index.numDocs());
         index.close();
 
         // Reopening it should still allow us to read its documents
-        var newIndexSameDir = new Indexer.Builder().indexDirectory(tempDir).appendMode().build();
+        var newIndexSameDir = new Indexer.Builder().dataDirectory(tempDir).appendMode().build();
         assertEquals(1, newIndexSameDir.numDocs());
         newIndexSameDir.close();
 
         // But opening should erase the old data
-        var destructiveIndex = new Indexer.Builder().indexDirectory(tempDir).createMode().build();
+        var destructiveIndex = new Indexer.Builder().dataDirectory(tempDir).createMode().build();
         assertEquals(0, destructiveIndex.numDocs());
     }
 }
