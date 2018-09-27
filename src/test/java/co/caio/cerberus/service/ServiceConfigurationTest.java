d290042bee98bc939ae1f08defbdc6b5968b69e4
Caio Romão <hello@caio.co>
2018-09-27 13:55:35 +0200

    Cleanup: use expression lambdas when possible

--- src/test/java/co/caio/cerberus/service/ServiceConfigurationTest.java
+++ src/test/java/co/caio/cerberus/service/ServiceConfigurationTest.java
@@ -11,38 +11,30 @@ class ServiceConfigurationTest {
   void validations() {
     assertThrows(
         ServiceConfiguration.ConfigurationException.class,
-        () -> {
-          ServiceConfiguration.create(new JsonObject());
-        });
+        () -> ServiceConfiguration.create(new JsonObject()));
     assertThrows(
         ServiceConfiguration.ConfigurationException.class,
-        () -> {
-          ServiceConfiguration.create(new JsonObject().put(CONFIG_SERVICE_PORT, -1));
-        });
+        () -> ServiceConfiguration.create(new JsonObject().put(CONFIG_SERVICE_PORT, -1)));
     assertThrows(
         ServiceConfiguration.ConfigurationException.class,
-        () -> {
-          ServiceConfiguration.create(new JsonObject().put(CONFIG_SERVICE_PORT, 70000));
-        });
+        () -> ServiceConfiguration.create(new JsonObject().put(CONFIG_SERVICE_PORT, 70000)));
     assertThrows(
         ServiceConfiguration.ConfigurationException.class,
-        () -> {
-          ServiceConfiguration.create(
-              new JsonObject().put(CONFIG_SERVICE_DATA_DIR, "/path/doesnt/exist"));
-        });
+        () ->
+            ServiceConfiguration.create(
+                new JsonObject().put(CONFIG_SERVICE_DATA_DIR, "/path/doesnt/exist")));
     assertThrows(
         ServiceConfiguration.ConfigurationException.class,
-        () -> {
-          ServiceConfiguration.create(new JsonObject().put(CONFIG_SERVICE_DATA_DIR, "/etc/passwd"));
-        });
+        () ->
+            ServiceConfiguration.create(
+                new JsonObject().put(CONFIG_SERVICE_DATA_DIR, "/etc/passwd")));
     assertDoesNotThrow(
-        () -> {
-          ServiceConfiguration.create(
-              new JsonObject()
-                  .put(CONFIG_SERVICE_PORT, 80)
-                  .put(CONFIG_SERVICE_DATA_DIR, "/tmp/")
-                  .put(CONFIG_SERVICE_SSL, true)
-                  .put(CONFIG_HEALTH_NUM_DOCS, 1));
-        });
+        () ->
+            ServiceConfiguration.create(
+                new JsonObject()
+                    .put(CONFIG_SERVICE_PORT, 80)
+                    .put(CONFIG_SERVICE_DATA_DIR, "/tmp/")
+                    .put(CONFIG_SERVICE_SSL, true)
+                    .put(CONFIG_HEALTH_NUM_DOCS, 1)));
   }
 }
