e3cec3fcc7e90277975d613d812ab115261cd542
Caio Romão <hello@caio.co>
2019-01-13 18:25:23 +0100

    Make BootApplicationTests pass again
    Now that there's a application.properties in the test resources
    classpath the whole configuration chain happens and this test
    started failing... This patch simply makes it pass by providing
    dummy/unused properties and using Util's indexer to build the
    required Searcher bean.

--- src/test/java/co/caio/cerberus/boot/BootApplicationTests.java
+++ src/test/java/co/caio/cerberus/boot/BootApplicationTests.java
@@ -1,10 +1,22 @@
 package co.caio.cerberus.boot;
 
+import co.caio.cerberus.Util;
+import co.caio.cerberus.search.Searcher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 class BootApplicationTests {
+
+  @TestConfiguration
+  static class SampleSettings {
+    @Bean
+    Searcher getSearcher() {
+      return Util.getTestIndexer().buildSearcher();
+    }
+  }
 
   @Test
   void contextLoads() {}
