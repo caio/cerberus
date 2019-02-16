b3ca33b5607b3c292826d49cec235aaf0b4e89df
Caio Romão <hello@caio.co>
2019-02-16 17:31:29 -0200

    Add a very basic (and static) sidebar

--- src/main/java/co/caio/cerberus/boot/ModelView.java
+++ src/main/java/co/caio/cerberus/boot/ModelView.java
@@ -3,9 +3,11 @@ package co.caio.cerberus.boot;
 import co.caio.cerberus.db.RecipeMetadata;
 import co.caio.cerberus.db.RecipeMetadataDatabase;
 import co.caio.cerberus.model.SearchQuery;
+import co.caio.cerberus.model.SearchQuery.SortOrder;
 import co.caio.cerberus.model.SearchResult;
 import co.caio.cerberus.model.SearchResultRecipe;
 import co.caio.tablier.model.ErrorInfo;
+import co.caio.tablier.model.FilterInfo;
 import co.caio.tablier.model.PageInfo;
 import co.caio.tablier.model.RecipeInfo;
 import co.caio.tablier.model.SearchFormInfo;
@@ -47,8 +49,6 @@ class ModelView {
       new PageInfo.Builder().title(ERROR_PAGE_TITLE).build();
   private static final String DEFAULT_UNKNOWN_ERROR_SUBTITLE = "Unknown Error Cause";
 
-  private static final SidebarInfo emptySidebar = new SidebarInfo.Builder().build();
-
   private final int pageSize;
   private final RecipeMetadataDatabase db;
   private final CircuitBreaker breaker;
@@ -86,7 +86,6 @@ class ModelView {
 
       var searchBuilder =
           new SearchResultsInfo.Builder()
-              .sidebar(emptySidebar)
               .paginationStart(query.offset() + 1)
               .paginationEnd(result.recipes().size() + query.offset())
               .numMatching(result.totalHits());
@@ -104,6 +103,74 @@ class ModelView {
       }
 
       searchBuilder.recipes(renderRecipes(result.recipes()));
+
+      // TODO interpret SearchQuery filters so that we can display an
+      //      explanation such as "searched for avocado, with 6 to 10 ingredients
+      //      and cook time less than 15 minutes.
+      // TODO figure out active / undo filtering
+      // XXX maybe add a link to reset filters
+
+      var sbb = new SidebarInfo.Builder();
+
+      for (SortOrder order : SortOrder.values()) {
+        // FIXME proper sort order labels
+        sbb.addSortOption(order.toString(), order.toString(), order.equals(query.sort()));
+      }
+
+      // Filters always lead to the first page
+      // FIXME test
+      uriBuilder.replaceQueryParam("page");
+
+      // We need to clone the builder for every group of filters otherwise
+      // we'll end up overwriting previous selections or poisoning the
+      // uris for the subsequent filters
+      var ub = uriBuilder.cloneBuilder();
+      sbb.addFilters(
+          new FilterInfo.Builder()
+              .showCounts(false)
+              .name("Limit Ingredients")
+              .addOption("Less than 5", ub.replaceQueryParam("ni", "5").build().toUriString(), 0)
+              .addOption("6 to 10", ub.replaceQueryParam("ni", "6,10").build().toUriString(), 0)
+              .addOption(
+                  "More than 10", ub.replaceQueryParam("ni", "11,0").build().toUriString(), 0)
+              .build());
+
+      ub = uriBuilder.cloneBuilder();
+      sbb.addFilters(
+          new FilterInfo.Builder()
+              .showCounts(false)
+              .name("Limit Cook Time")
+              .addOption(
+                  "Up to 15 minutes", ub.replaceQueryParam("ct", "15").build().toUriString(), 0)
+              .addOption(
+                  "15 to 30 minutes", ub.replaceQueryParam("ct", "15,30").build().toUriString(), 0)
+              .addOption(
+                  "30 to 60 minutes", ub.replaceQueryParam("ct", "30,60").build().toUriString(), 0)
+              .addOption(
+                  "One hour or more", ub.replaceQueryParam("ct", "60,0").build().toUriString(), 0)
+              .build());
+
+      var nutritionFilterBuilder =
+          new FilterInfo.Builder().showCounts(false).name("Limit Nutrition (per serving)");
+
+      ub = uriBuilder.cloneBuilder();
+      nutritionFilterBuilder
+          .addOption("Up to 200 kcal", ub.replaceQueryParam("n_k", "200").build().toUriString(), 0)
+          .addOption("Up to 500 kcal", ub.replaceQueryParam("n_k", "500").build().toUriString(), 0);
+
+      nutritionFilterBuilder
+          .addOption(
+              "Up to 10g of Fat",
+              ub.cloneBuilder().replaceQueryParam("n_f", "10").build().toUriString(),
+              0)
+          .addOption(
+              "Up to 30g of Carbs",
+              ub.cloneBuilder().replaceQueryParam("n_c", "30").build().toUriString(),
+              0);
+
+      sbb.addFilters(nutritionFilterBuilder.build());
+
+      searchBuilder.sidebar(sbb.build());
 
       return Search.template(defaultSite, defaultSearchPage, searchForm, searchBuilder.build());
     }
