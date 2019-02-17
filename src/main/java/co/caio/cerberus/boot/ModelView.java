ff5fc11ec653ca95a85c2b20d71c940ec96aa1ce
Caio Romão <hello@caio.co>
2019-02-17 11:22:14 -0300

    Extract sidebar logic into SidebarComponent
    TODO: Tests

--- src/main/java/co/caio/cerberus/boot/ModelView.java
+++ src/main/java/co/caio/cerberus/boot/ModelView.java
@@ -1,5 +1,6 @@
 package co.caio.cerberus.boot;
 
+import co.caio.cerberus.boot.SidebarComponent.SortOptionSpec;
 import co.caio.cerberus.db.RecipeMetadata;
 import co.caio.cerberus.db.RecipeMetadataDatabase;
 import co.caio.cerberus.model.SearchQuery;
@@ -55,6 +56,7 @@ class ModelView {
   private final int pageSize;
   private final RecipeMetadataDatabase db;
   private final CircuitBreaker breaker;
+  private final SidebarComponent sidebarComponent = new SidebarComponent();
 
   ModelView(
       @Qualifier("searchPageSize") int pageSize,
@@ -72,92 +74,6 @@ class ModelView {
       return Index.template(unstableSite, defaultIndexPage, defaultSearchForm);
     }
   }
-
-  static class SortOptionSpec {
-    private final String name;
-    private final SortOrder order;
-    private final String queryValue;
-
-    SortOptionSpec(String name, SortOrder order, String queryValue) {
-      this.name = name;
-      this.order = order;
-      this.queryValue = queryValue;
-    }
-
-    FilterOption buildOption(UriComponentsBuilder uriBuilder, SortOrder active) {
-
-      var href = uriBuilder.replaceQueryParam("sort", queryValue);
-
-      return new FilterInfo.FilterOption.Builder()
-          .name(name)
-          .isActive(active.equals(order))
-          .href(href.build().toUriString())
-          .build();
-    }
-  }
-
-  static class RangeOptionSpec {
-
-    private final String name;
-    private final int start;
-    private final int end;
-    private final String queryName;
-    private final String queryValue;
-
-    RangeOptionSpec(String name, int start, int end, String queryName) {
-      this.name = name;
-      this.start = start;
-      this.end = end;
-      this.queryName = queryName;
-      this.queryValue = String.format("%d,%d", start, end == Integer.MAX_VALUE ? 0 : end);
-    }
-
-    FilterOption buildOption(UriComponentsBuilder uriBuilder, RangedSpec selected) {
-      var isActive = selected.start() == start && selected.end() == end;
-
-      var href =
-          isActive
-              ? uriBuilder.replaceQueryParam(queryName)
-              : uriBuilder.replaceQueryParam(queryName, queryValue);
-
-      return new FilterInfo.FilterOption.Builder()
-          .name(name)
-          .isActive(isActive)
-          .href(href.build().toUriString())
-          .build();
-    }
-  }
-
-  private static final List<SortOptionSpec> sortOptions =
-      List.of(
-          new SortOptionSpec("Relevance", SortOrder.RELEVANCE, "relevance"),
-          new SortOptionSpec("Fastest to Cook", SortOrder.TOTAL_TIME, "total_time"),
-          new SortOptionSpec("Least Ingredients", SortOrder.NUM_INGREDIENTS, "num_ingredients"),
-          new SortOptionSpec("Calories", SortOrder.CALORIES, "calories"));
-
-  private static final List<RangeOptionSpec> ingredientFilterOptions =
-      List.of(
-          new RangeOptionSpec("Up to 5", 0, 5, "ni"),
-          new RangeOptionSpec("From 6 to 10", 6, 10, "ni"),
-          new RangeOptionSpec("More than 10", 10, Integer.MAX_VALUE, "ni"));
-
-  private static final List<RangeOptionSpec> totalTimeFilterOptions =
-      List.of(
-          new RangeOptionSpec("Up to 15 minutes", 0, 15, "tt"),
-          new RangeOptionSpec("From 15 to 30 minutes", 15, 30, "tt"),
-          new RangeOptionSpec("From 30 to 60 minutes", 30, 60, "tt"),
-          new RangeOptionSpec("One hour or more", 60, Integer.MAX_VALUE, "tt"));
-
-  private static final List<RangeOptionSpec> caloriesFilterOptions =
-      List.of(
-          new RangeOptionSpec("Up to 200 kcal", 0, 200, "n_k"),
-          new RangeOptionSpec("Up to 500 kcal", 0, 500, "n_k"));
-
-  private static final List<RangeOptionSpec> fatFilterOptions =
-      List.of(new RangeOptionSpec("Up to 10g of Fat", 0, 10, "n_f"));
-
-  private static final List<RangeOptionSpec> carbsFilterOptions =
-      List.of(new RangeOptionSpec("Up to 30g of Carbs", 0, 30, "n_c"));
 
   RockerModel renderSearch(
       SearchQuery query, SearchResult result, UriComponentsBuilder uriBuilder) {
@@ -196,82 +112,13 @@ class ModelView {
       // TODO interpret SearchQuery filters so that we can display an
       //      explanation such as "searched for avocado, with 6 to 10 ingredients
       //      and cook time less than 15 minutes.
-      // TODO figure out active / undo filtering
       // XXX maybe add a link to reset filters
-
-      var sbb = new SidebarInfo.Builder();
 
       // Filters always lead to the first page
       // FIXME test
       uriBuilder.replaceQueryParam("page");
 
-      // We need to clone the builder for every group of filters otherwise
-      // we'll end up overwriting previous selections or poisoning the
-      // uris for the subsequent filters
-      // SORTING
-      var ub = uriBuilder.cloneBuilder();
-
-      var sortInfoBuilder =
-          new FilterInfo.Builder().showCounts(false).isRemovable(false).name("Sort recipes by");
-
-      for (SortOptionSpec spec : sortOptions) {
-        sortInfoBuilder.addOptions(spec.buildOption(ub, query.sort()));
-      }
-
-      sbb.addFilters(sortInfoBuilder.build());
-
-      // INGREDIENTS
-      ub = uriBuilder.cloneBuilder();
-      var activeIng = query.numIngredients().orElse(unselectedRange);
-
-      var ingredientsFilterInfoBuilder =
-          new FilterInfo.Builder().showCounts(false).name("Limit Ingredients");
-
-      for (var spec : ingredientFilterOptions) {
-        ingredientsFilterInfoBuilder.addOptions(spec.buildOption(ub, activeIng));
-      }
-
-      sbb.addFilters(ingredientsFilterInfoBuilder.build());
-
-      // TOTAL TIME
-      ub = uriBuilder.cloneBuilder();
-      var activeTT = query.totalTime().orElse(unselectedRange);
-      var timeFilterInfoBuilder =
-          new FilterInfo.Builder().showCounts(false).name("Limit Total Time");
-
-      for (var spec : totalTimeFilterOptions) {
-        timeFilterInfoBuilder.addOptions(spec.buildOption(ub, activeTT));
-      }
-
-      sbb.addFilters(timeFilterInfoBuilder.build());
-
-      // NUTRITION
-      // TODO We use mixed queryValues in this filter, make sure we're
-      //      not poisoning the urls
-      var activeKcal = query.calories().orElse(unselectedRange);
-      var activeFat = query.fatContent().orElse(unselectedRange);
-      var activeCarbs = query.carbohydrateContent().orElse(unselectedRange);
-      var nutritionFilterInfoBuilder =
-          new FilterInfo.Builder().showCounts(false).name("Limit Nutrition (per serving)");
-
-      ub = uriBuilder.cloneBuilder();
-      for (var spec : caloriesFilterOptions) {
-        nutritionFilterInfoBuilder.addOptions(spec.buildOption(ub, activeKcal));
-      }
-
-      ub = uriBuilder.cloneBuilder();
-      for (var spec : fatFilterOptions) {
-        nutritionFilterInfoBuilder.addOptions(spec.buildOption(ub, activeFat));
-      }
-
-      ub = uriBuilder.cloneBuilder();
-      for (var spec : carbsFilterOptions) {
-        nutritionFilterInfoBuilder.addOptions(spec.buildOption(ub, activeCarbs));
-      }
-
-      sbb.addFilters(nutritionFilterInfoBuilder.build());
-
-      searchBuilder.sidebar(sbb.build());
+      searchBuilder.sidebar(sidebarComponent.build(query,uriBuilder));
 
       return Search.template(defaultSite, defaultSearchPage, searchForm, searchBuilder.build());
     }
