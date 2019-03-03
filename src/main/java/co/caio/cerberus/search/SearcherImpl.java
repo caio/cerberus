98613d3d2d6a6398cedf766ffcb249b98a4b8c42
Caio Romão <hello@caio.co>
2019-03-03 12:32:50 +0100

    Drop QueryInterpreter: Move logic inside SearchImpl
    It bothers me that I pass around a bunch of stuff just so that
    I can create this QueryInterpreter that I don't have any intention
    of providing alternative implementation for now.
    
    The SearchPolicy addition in particular was what flagged that the
    setup was quite awkward. Next step: Use inheritance to build a
    searcher that handles policies instead of the current error
    prone `searchPolicy != null`.

--- src/main/java/co/caio/cerberus/search/SearcherImpl.java
+++ src/main/java/co/caio/cerberus/search/SearcherImpl.java
@@ -1,22 +1,55 @@
 package co.caio.cerberus.search;
 
+import static co.caio.cerberus.search.IndexField.*;
+
 import co.caio.cerberus.lucene.FloatAssociationsThresholdCount;
 import co.caio.cerberus.model.FacetData;
 import co.caio.cerberus.model.SearchQuery;
+import co.caio.cerberus.model.SearchQuery.SortOrder;
 import co.caio.cerberus.model.SearchResult;
 import java.io.IOException;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.FloatPoint;
+import org.apache.lucene.document.IntPoint;
 import org.apache.lucene.facet.FacetResult;
 import org.apache.lucene.facet.FacetsCollector;
 import org.apache.lucene.facet.taxonomy.TaxonomyReader;
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.Sort;
+import org.apache.lucene.search.SortField;
+import org.apache.lucene.search.SortField.Type;
 
 public class SearcherImpl implements Searcher {
+
+  private static final Sort sortNumIngredients = integerSorterWithDefault(NUM_INGREDIENTS);
+  private static final Sort sortPrepTime = integerSorterWithDefault(PREP_TIME);
+  private static final Sort sortCookTime = integerSorterWithDefault(COOK_TIME);
+  private static final Sort sortTotalTime = integerSorterWithDefault(TOTAL_TIME);
+  private static final Sort sortCalories = integerSorterWithDefault(CALORIES);
+
   private final IndexSearcher indexSearcher;
   private final TaxonomyReader taxonomyReader;
-  private final QueryInterpreter interpreter;
   private final IndexConfiguration indexConfiguration;
   private final SearchPolicy searchPolicy;
+  private final FulltextQueryParser queryParser;
+
+  SearcherImpl(Builder builder) {
+    // XXX create a worker pool to assign for the searcher maybe
+    indexSearcher = new IndexSearcher(builder.getIndexReader());
+    taxonomyReader = builder.getTaxonomyReader();
+    indexConfiguration = builder.getIndexConfiguration();
+    searchPolicy = builder.getSearchPolicy();
+    queryParser = new FulltextQueryParser(indexConfiguration.getAnalyzer());
+  }
+
+  private static Sort integerSorterWithDefault(String fieldName) {
+    var field = new SortField(fieldName, Type.INT);
+    field.setMissingValue(Integer.MAX_VALUE);
+    return new Sort(field, SortField.FIELD_SCORE);
+  }
 
   public SearchResult search(SearchQuery query) {
     try {
@@ -30,25 +63,15 @@ public class SearcherImpl implements Searcher {
     return indexSearcher.getIndexReader().numDocs();
   }
 
-  SearcherImpl(Builder builder) {
-    // XXX create a worker pool to assign for the searcher maybe
-    indexSearcher = new IndexSearcher(builder.getIndexReader());
-    taxonomyReader = builder.getTaxonomyReader();
-    indexConfiguration = builder.getIndexConfiguration();
-    searchPolicy = builder.getSearchPolicy();
-
-    interpreter = new QueryInterpreter(indexConfiguration, searchPolicy);
-  }
-
   private SearchResult _search(SearchQuery query) throws IOException {
     var fc = new FacetsCollector();
 
     var result =
         FacetsCollector.search(
             indexSearcher,
-            interpreter.toLuceneQuery(query),
+            toLuceneQuery(query),
             query.offset() + query.maxResults(),
-            interpreter.toLuceneSort(query),
+            toLuceneSort(query.sort()),
             fc);
     var builder = new SearchResult.Builder().totalHits(result.totalHits);
 
@@ -83,5 +106,115 @@ public class SearcherImpl implements Searcher {
       facetDataBuilder.addChild(fr.labelValues[i].label, fr.labelValues[i].value.longValue());
     }
     sb.addFacets(facetDataBuilder.build());
+  }
+
+  Query parseFulltext(String fulltext) {
+    return queryParser.parse(fulltext);
+  }
+
+  Sort toLuceneSort(SortOrder sortOrder) {
+    switch (sortOrder) {
+      case RELEVANCE:
+        return Sort.RELEVANCE;
+      case NUM_INGREDIENTS:
+        return sortNumIngredients;
+      case PREP_TIME:
+        return sortPrepTime;
+      case COOK_TIME:
+        return sortCookTime;
+      case TOTAL_TIME:
+        return sortTotalTime;
+      case CALORIES:
+        return sortCalories;
+      default:
+        throw new IllegalStateException(String.format("Unhandled sort order: %s", sortOrder));
+    }
+  }
+
+  Query toLuceneQuery(SearchQuery searchQuery) {
+    var queryBuilder = new BooleanQuery.Builder();
+
+    searchQuery
+        .fulltext()
+        .ifPresent(
+            fulltext -> {
+              var parsed = parseFulltext(fulltext);
+
+              if (searchPolicy != null) {
+                searchPolicy.inspectParsedFulltextQuery(parsed);
+              }
+
+              queryBuilder.add(parsed, Occur.MUST);
+            });
+
+    searchQuery
+        .numIngredients()
+        .ifPresent(
+            range ->
+                queryBuilder.add(
+                    IntPoint.newRangeQuery(NUM_INGREDIENTS, range.start(), range.end()),
+                    Occur.MUST));
+
+    searchQuery
+        .cookTime()
+        .ifPresent(
+            range ->
+                queryBuilder.add(
+                    IntPoint.newRangeQuery(COOK_TIME, range.start(), range.end()), Occur.MUST));
+
+    searchQuery
+        .prepTime()
+        .ifPresent(
+            range ->
+                queryBuilder.add(
+                    IntPoint.newRangeQuery(PREP_TIME, range.start(), range.end()), Occur.MUST));
+
+    searchQuery
+        .totalTime()
+        .ifPresent(
+            range ->
+                queryBuilder.add(
+                    IntPoint.newRangeQuery(TOTAL_TIME, range.start(), range.end()), Occur.MUST));
+
+    searchQuery
+        .calories()
+        .ifPresent(
+            range ->
+                queryBuilder.add(
+                    IntPoint.newRangeQuery(CALORIES, range.start(), range.end()), Occur.MUST));
+
+    searchQuery
+        .fatContent()
+        .ifPresent(
+            range ->
+                queryBuilder.add(
+                    IntPoint.newRangeQuery(FAT_CONTENT, range.start(), range.end()), Occur.MUST));
+
+    searchQuery
+        .proteinContent()
+        .ifPresent(
+            range ->
+                queryBuilder.add(
+                    IntPoint.newRangeQuery(PROTEIN_CONTENT, range.start(), range.end()),
+                    Occur.MUST));
+
+    searchQuery
+        .carbohydrateContent()
+        .ifPresent(
+            range ->
+                queryBuilder.add(
+                    IntPoint.newRangeQuery(CARBOHYDRATE_CONTENT, range.start(), range.end()),
+                    Occur.MUST));
+
+    searchQuery
+        .dietThreshold()
+        .forEach(
+            (diet, score) ->
+                queryBuilder.add(
+                    FloatPoint.newRangeQuery(
+                        IndexField.getFieldNameForDiet(diet), score, Float.MAX_VALUE),
+                    Occur.MUST));
+
+    return queryBuilder.build();
   }
 }
